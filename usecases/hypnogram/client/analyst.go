86fd3ff0ef5d9b24fb16a7e5efc0647619361444
hosseinabdinf <hossein.abdinasibfar@tuni.fi>
2024-05-03 17:53:50 +0300

    refactor + add uc2 dataset

---
+++ usecases/hypnogram/client/analyst.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"SPADE"
+	pb "SPADE/spadeProto"
+	"SPADE/usecases/hypnogram"
+	"SPADE/utils"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"math/big"
+	"time"
+)
+
+type Analyst struct {
+	id    int
+	q     *big.Int
+	g     *big.Int
+	spade *SPADE.SPADE
+	mpk   []*big.Int
+}
+
+func NewAnalyst(q, g *big.Int, mpk []*big.Int) *Analyst {
+	return &Analyst{
+		id:    1,
+		q:     q,
+		g:     g,
+		spade: nil,
+		mpk:   mpk,
+	}
+}
+
+func main() {
+	start := time.Now()
+
+	pbHandler := hypnogram.NewPBHandler()
+
+	log.Println(">>> Analyst starts connecting to the server..")
+	addr := fmt.Sprintf("localhost:%d", utils.Port)
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// proto buffer init
+	a := pb.NewCuratorClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// SPADE calls for Analysts here :)
+	q, g, mpk, err := pbHandler.ReadPublicParams(a.GetPublicParams(ctx, &pb.PublicParamsReq{}))
+	if err != nil {
+		log.Fatalf("could not fetch the public parameters: %v", err)
+	}
+
+	// create a new Analyst
+	// create an instance of SPADE with same public params of server
+	analyst := NewAnalyst(q, g, mpk)
+	spd := SPADE.NewSpade(q, g, hypnogram.MaxVecSize)
+	analyst.spade = spd
+
+	utils.PrintBigIntHex("q", q)
+	utils.PrintBigIntHex("g", g)
+
+	// send a query for value(1) and user-id(1) to the server
+	req := &pb.AnalystReq{
+		Id:    0,
+		Value: 1,
+	}
+
+	// get the unmarshal values
+	dkv, cts, err := pbHandler.ReadDecryptionKey(a.Query(ctx, req))
+	if err != nil {
+		log.Fatalf("could not send the request: %v", err)
+	}
+
+	// partially decrypt the ciphertext vector using dkv to get the result for query
+	results := spd.Decrypt(dkv, int(req.Value), cts)
+
+	end := time.Now()
+	elapsed := end.Sub(start)
+	log.Printf("Analyst finished in %s", elapsed)
+	// !!! WARNING !!!!
+	// THIS IS TO VERIFY THE results and proof the correctness of the protocol
+	// we are not going to do this in a real world application, Hopefully :)
+	// read the original user's data from file
+	datasetDir := "../dataset/"
+	fileName := "b000101.txt"
+	data := utils.AddPadding(hypnogram.PaddingItem, hypnogram.MaxVecSize, utils.ReadFile(datasetDir+fileName))
+	//log.Println(results)
+	utils.VerifyResults(data, results, int(req.Value))
+	log.Println(">>> Analyst's operations are done!")
+}
---
+++ usecases/hypnogram/config.go
@@ -0,0 +1,25 @@
+package hypnogram
+
+// Database Configuration
+const (
+	DbName = "database.sqlite"
+	TbName = "users_cipher"
+)
+
+// SPADE configuration
+const (
+	// NumUsers for now let's just assume that each user is
+	// going to encrypt a single file, so NumUsers is going
+	// to be the number of hypnogram files
+	NumUsers = 10
+	// MaxVecSize should be defined to a fixed number, where
+	// the system is going to work with it by running spade's
+	// setup phase. Actually the length of mpk and msk is set
+	// to this value
+	MaxVecSize = 1000
+	// PaddingItem basically it must be anything larger than
+	// the maximum value of biggest element of the vector of
+	// hypnograms, in this case for us is 10, so any x > 10 works
+	// why it is 22? just for fun :)
+	PaddingItem = 22
+)

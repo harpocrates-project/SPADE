a16580b00c74030c6c0b23bbdb32cc18c1b877ff
jmz-b <jmz-b@users.noreply.github.com>
2025-05-02 15:51:19 +0200

    Hypnogram usecase analyst now takes a query and results file parameters

--- usecases/hypnogram/cmd/analyst/main.go
+++ usecases/hypnogram/cmd/analyst/main.go
@@ -6,35 +6,57 @@ import (
 	"os"
         "fmt"
         "strconv"
+	"math/big"
+        "encoding/csv"
+	"strings"
 )
 
 // InitAnalyst start the SPADE analyst using hypnogram use-case configuration
-func InitAnalyst(serverAddr string, userID int64, queryValue int64) {
+func InitAnalyst(serverAddr string, userID int64, queryValue int64, resultsFile string) {
 	config := models.NewConfig(DbName, TbName, NumUsers, MaxVecSize, PaddingItem, TimeOut, MaxMsgSize)
 	// start analyst entity, send a req to server for getting the cipher belongs to
 	// the user with userID and decrypt it for the specific query value queryValue
 	queryValue, results := models.StartAnalyst(serverAddr, config, userID, queryValue)
+	
+	count := 0
+	one := big.NewInt(1)
+	for _, v := range results {
+		if v.Cmp(one) == 0 {
+			count++
+		}
+	}
+	
+	file, err := os.Create(resultsFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-	// !!! WARNING !!!!
-	// THIS IS TO VERIFY THE results and proof the correctness of the protocol
-	// we are not going to do this in a real world application, Hopefully :)
-	// read the original user's data from file and compare it with the results
-	// datasetDir := "./dataset/"
-	// fileName := "b000101.txt"
-	//data := utils.AddPadding(config.PaddingItem, config.MaxVecSize, utils.ReadHypnogramFile(datasetDir+fileName))
-	// utils.VerifyResults(data, results, int(queryValue))
-	// log.Println(">>> Analyst's operations are done!")
+        wr := csv.NewWriter(file)
+	defer wr.Flush()
 
-	log.Println("Decrypted Data:", results)
+    	st := strings.Fields(strings.Trim(fmt.Sprint(results), "[]"))
+	wr.Write(st)
+
+	fmt.Printf("Count: %d\n", count)
 }
 
 func main() {
 	serverAddr := os.Args[1]
 	userID, err := strconv.ParseInt(os.Args[2], 10, 64)
+
     	if err != nil {
-		fmt.Println("Error converting user id to 64bit integer", err)
+		log.Println("Error converting user id to 64bit integer", err)
 		panic(err)
     	}
-	// Why 3?
-	InitAnalyst(serverAddr, userID, 3)
+
+	queryValue, err := strconv.ParseInt(os.Args[3], 10, 64)
+    	if err != nil {
+		log.Println("Error converting user id to 64bit integer", err)
+		panic(err)
+    	}
+
+	resultsFile := os.Args[4]
+
+	InitAnalyst(serverAddr, userID, queryValue, resultsFile)
 }
